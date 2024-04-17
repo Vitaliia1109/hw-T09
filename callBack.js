66513301f3e7f879553d4ca72f09e2f0a054aab0
Vitaliia1109 <vitusechka@ukr.net>
2024-04-17 15:30:25 +0100

    comments

--- callBack.js
+++ callBack.js
@@ -1,21 +1,28 @@
 
+// Initialize variables to store the interval ID and counter
 let intervalID;
 let counter = 0;
 
+// Function to start the counter
 let startCounter = function() {
+    // Set interval to increment counter every 1000ms (1 second)
     intervalID = setInterval(() => { 
         counter++;
-        console.log(counter);
+        console.log(counter); // Output the current count to the console
     }, 1000);
 }
 
+// Function to stop the counter
 function stopCounter() {
-    clearInterval(intervalID);
+    clearInterval(intervalID); // Clear the interval to stop counting
 }
 
+// Get references to the start and stop buttons in the HTML document
 let startButton = document.getElementById('start');
 let stopButton = document.getElementById('stop');
 
+// Add event listener to the start button to begin counting when clicked
 startButton.addEventListener("click", startCounter);
 
+// Add event listener to the stop button to stop counting when clicked
 stopButton.addEventListener("click", stopCounter);
--- higherOrder.js
+++ higherOrder.js
@@ -1,20 +1,27 @@
+// Array containing words
 let words = ["wombat", "koala", "alpaca", "kangaroo", "k00kaburra", "panda", "deer", "pelican", "rhinoceros", "racoon"];
+
+// Function to check if a word has six letters
 function hasSixLetters(word) {
-    return word.length === 6;
+    return word.length === 6; // Returns true if the word has six letters, otherwise false
 }
 
-
+// Custom filter function to filter words based on a callback function
 function myFilterFunction(words, callback) {
-    
-    let filteredWords = [];
+    let filteredWords = []; // Array to store filtered words
 
+    // Iterate through each word in the input array
     for (let word of words) {
+        // Check if the callback function returns true for the current word
         if (callback(word)) {
-         filteredWords.push(word);
+            filteredWords.push(word); // Add the word to the filteredWords array if the callback returns true
         }
     }
-    return filteredWords;
+    return filteredWords; // Return the array of filtered words
 }
 
+// Call the custom filter function with the words array and the hasSixLetters callback function
 let filteredWordsArray = myFilterFunction(words, hasSixLetters);
+
+// Output the filtered words array to the console
 console.log(filteredWordsArray);
